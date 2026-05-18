41e310fae0a7ce09e0cfcb8f0ca0d860e2bc90da
tdangare <tdangare@ceic.uz.ac.zw>
2026-05-18 08:07:23 +0200

    change of server IP

--- AQI_Project_Sprint/src/main.cpp
+++ AQI_Project_Sprint/src/main.cpp
@@ -13,7 +13,7 @@ const char *WIFI_PASSWORD = "wkProg219!";
 
 // MQTT Configuration - High Availability Setup
 // Primary MQTT Broker (Raspberry Pi 1)
-const char *MQTT_PRIMARY_SERVER = "10.201.1.9";
+const char *MQTT_PRIMARY_SERVER = "10.144.4.20";
 const int MQTT_PRIMARY_PORT = 1883;
 
 // Secondary MQTT Broker (Raspberry Pi 2) - Backup
