66304e45b5673117ff5be87bc499562088646e86
tdangare <tdangare@ceic.uz.ac.zw>
2026-06-05 14:34:50 +0200

    Tweaking

--- AQI_Project_Sprint/src/main.cpp
+++ AQI_Project_Sprint/src/main.cpp
@@ -264,9 +264,9 @@ void loop() {
                     lcd.setCursor(0, 0);
                     lcd.print("T:"); lcd.print(temp, 1); lcd.print("C H:"); lcd.print(hum, 0); lcd.print("%   ");
                     lcd.setCursor(0, 1);
-                    lcd.print("PM2.5:"); lcd.print(pm25); lcd.print(" CO2:"); lcd.print(co2); lcd.print("  ");
+                    lcd.print(" CO2:"); lcd.print(co2); lcd.print("  ");lcd.print("PM2.5:"); lcd.print(pm25);
                     lcd.setCursor(0, 2);
-                    lcd.print("Lat:"); lcd.print(lat, 4); lcd.print(" "); lcd.print(netTime);
+                    lcd.print("TIME:  "); lcd.print(netTime);
                     
                     // Smart Diagnostic LCD Output
                     lcd.setCursor(0, 3);
